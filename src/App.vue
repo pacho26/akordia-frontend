e26ecb94f6e202e128f3ba536969d68fff6d16e1
pslovic <patrik.slovic@3ofthem.eu>
2022-08-26 22:13:15 +0200

    Add Poppins via css

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,8 @@ const { isLoading } = storeToRefs(spinnerStore);
 </template>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900');
+
 * {
   box-sizing: border-box;
   font-family: 'Poppins', sans-serif;
